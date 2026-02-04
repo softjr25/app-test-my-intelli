4377acb983d7dd5a8c0d585fd28e45a1a8303d14
softjr25 <soft.jr25@gmail.com>
2026-02-04 15:57:41 -0400

    format codigo

--- src/routes/api.php
+++ src/routes/api.php
@@ -16,10 +16,10 @@ use Illuminate\Http\Request;
 Route::post('register', 'API\AuthController@register');
 Route::post('login', 'API\AuthController@login');
 
-// Rutas que requieren que el usuario esté logueado
 Route::group(['middleware' => ['jwt.verify']], function() {
+
     Route::get('user', 'API\AuthController@getAuthenticatedUser');   
-    // Aquí irán las rutas de Libros y Autores después
+    
     Route::apiResource('authors', 'API\AuthorController');
     Route::apiResource('books', 'API\BookController');
 });
